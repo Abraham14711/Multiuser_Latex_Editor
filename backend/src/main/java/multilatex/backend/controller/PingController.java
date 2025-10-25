cbe723e8b3e840cb08e3247fb392810277afbb0d
nastosinka <osinkinanastasia@gmail.com>
2025-10-25 19:21:13 +0300

    feat: added sprig

---
+++ backend/src/main/java/multilatex/backend/controller/PingController.java
@@ -0,0 +1,18 @@
+package multilatex.backend.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class PingController {
+
+    @GetMapping("/ping/{id}")
+    public ResponseEntity<String> ping(@PathVariable Integer id) {
+        return ResponseEntity.status(HttpStatus.OK).body("pong");
+    }
+}
---
+++ backend/src/test/java/multilatex/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package multilatex.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
